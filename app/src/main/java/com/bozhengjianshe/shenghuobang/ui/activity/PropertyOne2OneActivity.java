6e916f7a496bf2d22df33ade8cd2d940e999313b
chenzhiwei152 <759499274@qq.com>
2018-03-17 22:08:44 +0800

    。。

---
+++ app/src/main/java/com/bozhengjianshe/shenghuobang/ui/activity/PropertyOne2OneActivity.java
@@ -0,0 +1,8 @@
+package com.bozhengjianshe.shenghuobang.ui.activity;
+
+/**
+ * Created by Administrator on 2018/3/17.
+ */
+
+public class PropertyOne2OneActivity {
+}
---
+++ app/src/main/java/com/bozhengjianshe/shenghuobang/ui/bean/PropertyOen2OneBean.java
@@ -0,0 +1,8 @@
+package com.bozhengjianshe.shenghuobang.ui.bean;
+
+/**
+ * Created by Administrator on 2018/3/17.
+ */
+
+public class PropertyOen2OneBean {
+}
